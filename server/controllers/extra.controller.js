a3a8436e4391291ced1d117d4648ff32e9856c37
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-02-14 20:26:11 +0545

    added the controller logic for the subscribe email validation, deals and recommendation items api

---
+++ server/controllers/extra.controller.js
@@ -0,0 +1,87 @@
+const EmailSubscribe = require('../models/subscribe.model');
+const handHeldProduct = require('../models/handHeld.model');
+const WearableTechProduct = require('../models/wearableTechProduct.model');
+const KitchenItem = require('../models/kitchenItem.mode');
+const Furniture = require('../models/furniture.model');
+
+/*
+  email subscribe logic
+*/
+const subscribe = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    // search for the existing email
+    const emailExists = await EmailSubscribe.findOne({
+      email: email.toLowerCase(),
+    });
+
+    if (emailExists) {
+      return res.status(400).json({
+        message: 'Email is already registered, try another one',
+        fulfilled: false,
+      });
+    }
+
+    // add new email to the database
+    const emailD = new EmailSubscribe({
+      email,
+    });
+
+    await emailD.save();
+
+    res
+      .status(201)
+      .json({ message: 'Email added to the list', fulfilled: true });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Server error while adding to the subscribe list',
+      fulfilled: false,
+    });
+  }
+};
+
+// home page recommendedItems list
+const recommendedItems = async (req, res) => {
+  try {
+    // get the random items from the list and then return as a json format
+    // include all the section
+    // for current we get the 5 random data form the two different collection
+    const [handHeldData, wearableData, kitchenData, furnitureData] =
+      await Promise.all([
+        handHeldProduct.aggregate([{ $sample: { size: 3 } }]),
+        WearableTechProduct.aggregate([{ $sample: { size: 3 } }]),
+        KitchenItem.aggregate([{ $sample: { size: 2 } }]),
+        Furniture.aggregate([{ $sample: { size: 2 } }]),
+      ]);
+
+    const randomRecommendedItems = [
+      ...handHeldData,
+      ...wearableData,
+      ...kitchenData,
+      ...furnitureData,
+    ];
+
+    res.status(200).json(randomRecommendedItems);
+  } catch (err) {
+    res.status(500).json({ message: err.message, success: false });
+  }
+};
+
+// home page deal and offer api
+const dealAndOffers = async (req, res) => {
+  try {
+    const [handHeldData, wearableData] = await Promise.all([
+      handHeldProduct.aggregate([{ $sample: { size: 2 } }]),
+      WearableTechProduct.aggregate([{ $sample: { size: 2 } }]),
+    ]);
+
+    const randomItems = [...handHeldData, ...wearableData];
+
+    res.status(200).json(randomItems);
+  } catch (err) {
+    res.status(500).json({ message: err.message, success: false });
+  }
+};
+
+module.exports = { subscribe, recommendedItems, dealAndOffers };
